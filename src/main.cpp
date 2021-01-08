04c8bb5a28d900e8c528cef57893b961a7ebee28
Dexter Maher <dexter@maher-family.net>
2021-01-07 18:47:36 -0800

    light color dependent on photoresistor value

--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,37 @@
 int red = 11;
 int green = 10;
 int blue = 9;
+int phRes = A0;
+float maxLight = 500.0;
  
  
 void setup() {
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
+  Serial.begin(9600);
  
 }
 
 void rgbColor(int redValue, int greenValue, int blueValue ){
 
-  digitalWrite(red,redValue);
-  digitalWrite(green,greenValue);
-  digitalWrite(blue,blueValue);
+  analogWrite(red,redValue);
+  analogWrite(green,greenValue);
+  analogWrite(blue,blueValue);
 
 }
+
+void colorShift(){
+  float lightValue = analogRead(phRes);
+  float percentageOfLight = lightValue / maxLight;
+  int blueGlow = (int)(255 - percentageOfLight * 255);
+  int redGlow = (int)(255 - blueGlow);
+
+  rgbColor(redGlow, 255, blueGlow);
+  delay(30);
+}
+
  
 void loop() {
-  rgbColor(125, 0, 0); // Red
-  delay(500);
-  rgbColor(0, 125, 0); // Green
-  delay(500);
-  rgbColor(0, 0, 125); // Blue
-  delay(500);
-  rgbColor(0, 125, 125); // yellow
-  delay(500);
-  rgbColor(125, 0, 125); // purple
-  delay(500);
-  rgbColor(125, 125, 125); // white
-  delay(500);
+  colorShift();
 }
