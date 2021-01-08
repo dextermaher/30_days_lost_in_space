ae97fe3d0ed1df96d8b5bf1b1839da649b261076
Dexter Maher <dexter@maher-family.net>
2021-01-07 17:13:57 -0800

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,32 @@
 #define onboard 13
 
 int sensorPin = A0;
-int onboardLED = 13;
-int sensorValue = 0;
-
+int ledPin = 13;
+int sensorValue = 0; 
+unsigned int batteryCapacity = 50000;
+unsigned int batteryLevel = 0;
+ 
+void PrintBatteryPercentage() {
+  Serial.print(((double)batteryLevel / (double)batteryCapacity)*100);
+  Serial.println("%");
+}
+ 
 void setup() {
-  pinMode(onboardLED, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 }
-
+ 
 void loop() {
-  sensorValue = analogRead(sensorPin);
-  Serial.println(sensorValue);
-  digitalWrite(onboardLED, HIGH);
-  delay(sensorValue);
-  digitalWrite(onboardLED, LOW);
-  delay(sensorValue);
+  sensorValue = analogRead(sensorPin); 
+  batteryLevel += sensorValue;
+ 
+  if(batteryLevel >= batteryCapacity) {
+    Serial.println("FULLY CHARGED");
+    batteryLevel = batteryCapacity; 
+  }
+  else {
+    PrintBatteryPercentage();
+  }
+ 
+  delay(50);
 }
