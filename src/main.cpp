2bd09368984b74d2c1e78408315ae97561ca7a3d
Dexter Maher <dexter@maher-family.net>
2021-01-19 17:58:22 -0800

    we did something. I don't remember what

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
 #include <Keypad.h>
  
- int buzzer = 10;
+int buzzer = 8;
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
 const byte ROWS = 4;
 const byte COLS = 4;
 const byte PassLength = 4;
-char password[PassLength] = {'0','0','0','0',};
+char password[PassLength] = {'0','0','0','0'};
  
 char buttons[ROWS][COLS] = {
   {'1', '2', '3', 'A'},
@@ -13,19 +16,39 @@ char buttons[ROWS][COLS] = {
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
+ 
 byte rowPins[ROWS] = {5, 4, 3, 2};
-byte colPins[COLS] = {6, 7, 8, 9};
-
-
-int tones[ROWS][COLS] = {
-    {31, 93, 147, 208},
-    {247, 311, 370, 440},
-    {523, 587, 698, 880},
-    {1397, 2637, 3729, 4978}
-};
+byte colPins[COLS] = {6, 7, 12, 13};
  
- 
-Keypad customKeypad = Keypad(makeKeymap(buttons), rowPins, colPins, ROWS, COLS);
+Keypad customKeypad = Keypad(makeKeymap(buttons), rowPins, colPins, ROWS, COLS); 
+
+void RGB_color(int red_value, int green_value, int blue_value)
+{
+  analogWrite(redPin, red_value);
+  analogWrite(greenPin, green_value);
+  analogWrite(bluePin, blue_value);
+}
+
+void playSuccess(){
+    tone(buzzer, 370, 1000);
+    RGB_color(255,0,255);
+    delay(50);
+    noTone(buzzer);
+}
+
+void playError(){
+    tone(buzzer, 147, 500);
+    RGB_color(0, 255, 255);
+    delay(50);
+    noTone(buzzer);
+}
+
+void playInput() {
+  tone(buzzer, 880, 250);
+  RGB_color(255, 255, 0);
+  delay(50);
+  noTone(buzzer);
+}
  
 int unlockMode(){
     char customKey = customKeypad.getKey();
@@ -39,16 +62,24 @@ int unlockMode(){
         if(password[i] != customKey){
             Serial.println("WRONG PASSWORD");
             Serial.println(customKey);
+            playError();
             return -1;
         }
         Serial.print("*");
+        playInput();
     }
     Serial.print("");
     Serial.println("Device Unlocked!");
+    playSuccess();
     return 0;
 }
 
 void setup(){
+     pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+ 
+  RGB_color(255, 255, 255);
   Serial.begin(9600);
   Serial.println("Press * to set a new password OR # to access the system");
 }
@@ -57,6 +88,7 @@ void loop(){
     char customKey = customKeypad.getKey();
     if(customKey == '*'){
         int access = unlockMode();
+        playInput();
 
         if(access < 0){
             Serial.println("Access Denied. Can not change password without the old one or the default.");
@@ -74,6 +106,7 @@ void loop(){
             }
             Serial.println("");
             Serial.println("Password Succeddfully Changed.");
+            playSuccess();
         }
 
     }
@@ -84,6 +117,7 @@ void loop(){
             Serial.println("Access Denied.");
         }else{
             Serial.println("Welcome, authorized user, you may now begin using the system.");
+              playSuccess();
         }
     } 
 
