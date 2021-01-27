cc1b9f50ca6e8cd75cca5c00cc731ee576ddf102
Dexter Maher <dexter@maher-family.net>
2021-01-26 18:58:11 -0800

    dial and display

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,112 @@
 #include <Arduino.h>
 #include <TM1637Display.h>
-// Module connection pins (Digital Pins)
-#define CLK 2
-#define DIO 3
-// The amount of time (in milliseconds) between tests
-#define TEST_DELAY 2000
-TM1637Display display(CLK, DIO);
-void setup()
-{
+ 
+// Rotary Encoder Inputs
+#define CLK2 2
+#define DT2 3
+ 
+// Define the connections pins:
+#define CLK_DISP 6
+#define DIO_DISP 5
+ 
+// Create display object of type TM1637Display:
+TM1637Display display = TM1637Display(CLK_DISP, DIO_DISP);
+ 
+// Create array that turns all segments on:
+const uint8_t data[] = {0xff, 0xff, 0xff, 0xff};
+ 
+// Create array that turns all segments off:
+const uint8_t blank[] = {0x00, 0x00, 0x00, 0x00};
+ 
+// You can set the individual segments per digit to spell words or create other symbols:
+const uint8_t done[] = {
+  SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
+  SEG_C | SEG_E | SEG_G,                           // n
+  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G            // E
+};
+ 
+int counter = 0;
+int currentStateCLK;
+int lastStateCLK;
+String currentDir ="";
+ 
+
+void updateEncoder(){
+  // Read the current state of CLK
+  currentStateCLK = digitalRead(CLK2);
+ 
+  // If last and current state of CLK are different, then pulse occurred
+  // React to only 1 state change to avoid double count
+  if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
+ 
+    // If the DT state is different than the CLK state then
+    // the encoder is rotating CCW so decrement
+    if (digitalRead(DT2) != currentStateCLK) {
+      counter --;
+      currentDir ="CCW";
+    } else {
+      // Encoder is rotating CW so increment
+      counter ++;
+      currentDir ="CW";
+    }
+ 
+    Serial.print("Direction: ");
+    Serial.print(currentDir);
+    Serial.print(" | Counter: ");
+    Serial.println(counter);
+  }
+ 
+  // Remember last CLK state
+  lastStateCLK = currentStateCLK;
 }
-void loop()
-{
-display.setBrightness(0x0f);
-uint8_t data[] = { 0x0, 0x0, 0x0, 0x0 };
-display.setSegments(data);
-display.showNumberDec(23, false, 3, 1);
-delay(TEST_DELAY);
-display.setSegments(data);
-display.showNumberDec(353, true, 3, 1);
-delay(TEST_DELAY);
-display.setSegments(data);
-display.showNumberDec(1688, true, 4, 0);
-delay(TEST_DELAY);
+
+
+void setup() {
+ 
+  // Set encoder pins as inputs
+  pinMode(CLK2,INPUT);
+  pinMode(DT2,INPUT);
+ 
+  // Setup Serial Monitor
+  Serial.begin(9600);
+ 
+  // Read the initial state of CLK
+  lastStateCLK = digitalRead(CLK2);
+ 
+  // Clear the display:
+  display.clear();
+  delay(1000);
+  display.setBrightness(7);
+ 
+  // Call updateEncoder() when any high/low changed seen
+  // on interrupt 0 (pin 2), or interrupt 1 (pin 3)
+  attachInterrupt(0, updateEncoder, CHANGE);
+  attachInterrupt(1, updateEncoder, CHANGE);
 }
+ 
+void loop() {
+  display.showNumberDec(counter);
+  delay(50);
+ 
+  if(counter == 50) {
+    display.setSegments(data);
+    delay(3000);
+    display.clear();
+    counter += 5;
+  }
+ 
+ if(counter == 75) {
+    display.setSegments(data);
+    delay(3000);
+    display.clear();
+    counter += 5;
+  }
+ 
+  if(counter == 100) {
+    display.setSegments(done);
+    delay(1000000);
+  }
+ 
+}
+ 
